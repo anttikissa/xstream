ca47322b760da66e90bffdaa1a965b23eb4e84ab
Antti Sykäri <antti.sykari@gmail.com>
2014-08-17 16:03:22 +0300

    Do all kinds of horrible hacks to make tests work like they are supposed to do.
    Mostly make Stream.log() work, because it uses console.log directly.

---
+++ misc/test-after.js
@@ -0,0 +1,25 @@
+var realLog = console.log.bind(console);
+
+var TIME = 40;
+
+// TODO make use of a concept like 'test is done' and somehow arrange
+// following tests to only start after the previous test is done; this
+// is hacky and shaky and fragile and stupid, tests can leave behind all
+// kind of crap
+function runOneTest() {
+	console.log = makeConsole();
+	console.realLog = realLog;
+
+//	realLog('runOneTest,', tests.length, 'to go');
+	var test = tests.shift();
+
+	if (test) {
+		test();
+		setTimeout(runOneTest, TIME);
+	}
+
+};
+
+runOneTest();
+
+})();
---
+++ misc/test-before.js
@@ -0,0 +1,65 @@
+var util = require('util');
+var stream = require('../xstream');
+
+var consoleId = 1;
+
+(function() {
+var makeConsole = function() {
+	return (function() {
+		var id = consoleId++;
+
+		var recordingLog = function recordingLog() {
+			var args = Array.prototype.slice.apply(arguments);
+			var line = args.map(function(arg) {
+				if (typeof arg === 'string') {
+					return arg;
+				}
+				if (typeof arg === 'function') {
+					return Object.prototype.toString.apply(arg);
+				}
+				return util.format(arg);
+			}).join(' ');
+			recordingLog.lines.push(line);
+			console.realLog.call(console, '[console ' + id + ']', line);
+		};
+
+		recordingLog.lines = [];
+		return recordingLog
+
+	})();
+}
+
+function log(console) {
+	return console.log.bind(console);
+}
+
+function fail(message, stack, console) {
+	global.console.log(message);
+	if (console.test) {
+		global.console.log("Failing test:", console.test);
+	}
+	global.console.log("Original stacktrace: " + stack);
+	process.exit(1);
+}
+
+function later(f) {
+	setTimeout(f, 10);
+}
+
+function assertPrinted(log, str) {
+	var stack = new Error().stack;
+	if (str !== null) {
+		var actual = log.lines.shift();
+		if (actual !== str) {
+			fail("Expected '" + str + "', got '" + actual + "'", stack, console);
+		}
+	} else {
+		if (log.lines.length) {
+			fail("Expected nothing, console output was " +
+				JSON.stringify(log.lines), stack, console);
+		}
+	}
+}
+
+var tests = [];
+
